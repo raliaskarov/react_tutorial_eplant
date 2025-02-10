9df333b89e8903cc8af54ede64fa69d128db9625
Roman Aliaskarov <roman.aliaskarov@holcim.com>
2025-02-10 13:33:03 +0100

    gh config changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/react_tutorial_eplant",
+  base: "/react_tutorial_eplant/",
   plugins: [react()],
 })
